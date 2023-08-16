5564e0b628e678d6459761b523772971803f925e
knitinjaideep <knitinjaideep@gmail.com>
2023-08-16 06:38:09 -0700

    Finished Website
    Completed the 1st draft of the website

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,6 @@ const App = () => {
           </div>
           <About />
           <Experience />
-          <Tech />
-          <Works />
-          <Feedbacks />
           <div className="relative z-0">
             <Contact />
             <StarsCanvas />
